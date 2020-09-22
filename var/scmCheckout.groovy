b47df8f5f609c6ab7daad045549dad9aa4a58dce
ramarao05 <32882375+ramarao05@users.noreply.github.com>
2020-09-22 10:15:48 -0400

    Add files via upload

--- var/scmCheckout.groovy
+++ var/scmCheckout.groovy
@@ -1,11 +1,11 @@
 def call (Map stageParams) {
    checkout([
-	$class: 'GitSCM'
-	branches: [[name: stageParams.branch]]
-	doGenerateSubModuleConfigurations: false
-	userRemoteConfigs: [[
-		url: stageParams.url,
-		credentialsId: stageParams.credentials
-	]]
+		$class: 'GitSCM',
+		branches: [[name: stageParams.branch]],
+		doGenerateSubModuleConfigurations: false,
+		userRemoteConfigs: [[
+			url: stageParams.url,
+			credentialsId: stageParams.credentials
+		]]
+    ])
 }
-	
